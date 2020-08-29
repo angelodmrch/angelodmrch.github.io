12002b54afd572c456515559b5e27c6c14e9e97d
AngeloDmrch <angelodmrch@gmail.com>
2020-08-29 17:25:44 -0300

    update 29 08 20

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,3 @@
-function onKonamiCode(cb) {
-  var input = '';
-  var key = '38384040373937396665';
-  document.addEventListener('keydown', function (e) {
-    input += ("" + e.keyCode);
-    if (input === key) {
-      return cb();
-    }
-    if (!key.indexOf(input)) return;
-    input = ("" + e.keyCode);
-  });
-}
-
-onKonamiCode(function () {
-	
-  jQuery('.games').css('display', 'inline-block');
-
-});
-
 // ANIMATE ANCHOR 
 $("body").on('click', 'a', function(event){        
     var hash = this.hash;
