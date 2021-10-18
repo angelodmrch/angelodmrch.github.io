4d841645fdf4375c286a8565c33bdbd43b721418
AngeloDmrch <angelodmrch@gmail.com>
2021-10-18 09:33:50 -0300

    dfs

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var plugins	= require( "gulp-load-plugins" )({ lazy: false });
-var argv	= require( "minimist" )(process.argv.slice(1));
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var plugins = require( "gulp-load-plugins" )({ lazy: false });
+var fs      = require( "fs" );
+var argv    = require( "minimist" )(process.argv.slice(1));
 
 var config = {
-    bower: './bower_components',
-    bootstrapDir: './bower_components/bootstrap',
+    bootstrapDir: './bower_components/bootstrap-sass',
     publicDir: './assets',
 };
 
@@ -13,15 +14,14 @@ gulp.task('css', function() {
     return gulp.src( 'assets/sass/**/*.scss')
     .pipe( plugins.plumber({errorHandler: plugins.notify.onError("Erro ao compilar os styles, <%= error.message %>")}))
     .pipe(sass({
-        includePaths: [
-        config.bower
-        ],
+        includePaths: [config.bootstrapDir + '/assets/stylesheets'],
     }))
     .pipe( plugins.autoprefixer(['last 2 version', '> 1%', 'ie 8', 'ie 7'], {cascade: false}))
-	.pipe( plugins.rename({
-		suffix: '.min'
-	}))
+    .pipe( plugins.rename({
+        suffix: '.min'
+    }))
     .pipe( plugins.if( !( argv.d || argv.debug ), plugins.cleanCss() ) )
+    //.pipe( plugins.if( !( argv.d || argv.debug ), plugins.minifyCss() ) )
     .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
@@ -30,7 +30,8 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest(config.publicDir + '/fonts'));
 });
 
-gulp.task( 'watch', function() {
-	gulp.watch('assets/sass/**/*.scss', gulp.series('css'));
+gulp.task('watch', function() {
+    gulp.watch('assets/sass/**/*.scss', gulp.series('css'));
 });
 
+gulp.task('default', gulp.series('watch'));
